ac6d08302226babd8d51042d843cc612e5807670
asahi417 <asahi1992ushio@gmail.com>
2024-08-24 13:49:03 +0900

    fix the structure

--- experiment/all_queries/get_metric.py
+++ experiment/all_queries/get_metric.py
@@ -2,18 +2,25 @@ import os
 import json
 from glob import glob
 from pprint import pprint
+import pandas as pd
 from pse.eval_util import calculate_metric
 
 root_path = "./experiment/all_queries/output/result/*.label.json"
 output = "./experiment/all_queries/output/result/all_queries.result.json"
 top_k = [8, 16, 32, 64]
 metric = {}
+if os.path.exists(output):
+    with open(output, "r") as f:
+        metric.update(json.load(f))
+
 for file in glob(root_path):
     model_name = os.path.basename(file).replace(".label.json", "")
-    with open(file) as f:
-        search_result = json.load(f)
-    metric[model_name] = calculate_metric(search_result, top_k=top_k)
+    if model_name not in metric:
+        with open(file) as f:
+            search_result = json.load(f)
+        metric[model_name] = calculate_metric(search_result, top_k=top_k)
 pprint(metric)
 
 with open(output) as f:
     json.dump(metric, f)
+print(pd.DataFrame(metric).to_markdown())
--- pse/eval_util.py
+++ pse/eval_util.py
@@ -1,5 +1,5 @@
 from statistics import mean
-from typing import List, Optional, Dict, Any, Tuple
+from typing import List, Dict, Any, Tuple
 from sklearn.metrics import ndcg_score
 
 
@@ -17,28 +17,39 @@ def convert_search_result(search_result: Dict[str, List[Dict[str, Any]]]):
     return y_true, y_score, y_rank, query_id
 
 
-def binary_f1_score(y_true: List[List[int]], y_pred: List[List[float]], top_k: Optional[int] = None) -> Tuple[float, float, float]:
+def binary_f1_score(y_true: List[List[int]], y_pred: List[List[float]], top_k: int) -> Tuple[float, float, float]:
     full_precision, full_recall, full_f1 = [], [], []
     for label, prediction in zip(y_true, y_pred):
-        label = list(zip(*sorted(zip(label, prediction), key=lambda x: x[1], reverse=True)))
+        label = list(zip(*sorted(zip(label, prediction), key=lambda x: x[1], reverse=True)))[0]
         binary_label = [int(i > 0) for i in label]
         binary_label_top_k = binary_label[:top_k]
         precision = sum(binary_label_top_k)/len(binary_label_top_k)
         recall = sum(binary_label_top_k)/sum(binary_label)
-        f1 = 2 * precision * recall / (precision + recall)
+        if precision + recall == 0:
+            f1 = 0
+        else:
+            f1 = 2 * precision * recall / (precision + recall)
         full_precision.append(precision)
         full_recall.append(recall)
         full_f1.append(f1)
     return mean(full_precision), mean(full_recall), mean(full_f1)
 
 
-def calculate_metric(search_result: Dict[str, List[Dict[str, Any]]], top_k: Optional[List[int]] = None) -> Dict[str, float]:
+def calculate_metric(search_result: Dict[str, List[Dict[str, Any]]], top_k: List[int]) -> Dict[str, float]:
     y_true, y_score, y_rank, query_id = convert_search_result(search_result)
-    top_k = [None] if top_k is None else top_k
     metric = {}
+
+    def padding(x: list, length: int) -> list:
+        if len(x) >= length:
+            return x[:length]
+        return x + [0] * (length - len(x))
+
     for k in top_k:
-        metric[f"ndcg@{k}"] = ndcg_score(y_true, y_score, k=k)
         precision, recall, f1 = binary_f1_score(y_true, y_score, top_k=k)
+        metric[f"ndcg@{k}"] = ndcg_score(
+            [padding(i, k) for i in y_true],
+            [padding(i, k) for i in y_score],
+            k=k)
         metric[f"precision@{k}"] = precision
         metric[f"recall@{k}"] = recall
         metric[f"f1@{k}"] = f1
