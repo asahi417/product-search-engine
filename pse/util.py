84c9dfd8a6531dc9ec51359e3c39e4473f4049c5
asahi417 <asahi1992ushio@gmail.com>
2024-10-12 14:54:08 +0900

    init

--- pse/util.py
+++ pse/util.py
@@ -45,6 +45,10 @@ def get_semantic_search_result(
         chunk_size_save: int = 40000,
         query_chunk_size: int = 400,
         corpus_chunk_size: int = 400000) -> Dict[str, List[Dict[str, Union[str, float]]]]:
+    logger = get_logger(__name__)
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    logger.info(f"device: {device}")
+
     def load_index(output_dir) -> (Dict[int, str], List[str], torch.Tensor):
         with open(f"{output_dir}/index2id.json") as f:
             index2id = {int(_k): _v for _k, _v in json.load(f).items()}
@@ -62,14 +66,11 @@ def get_semantic_search_result(
         embedding = []
         for _, numpy_file in numpy_files:
             embedding.append(np_load(numpy_file))
-        embedding = torch.as_tensor(np.concatenate(embedding))
+        embedding = torch.as_tensor(np.concatenate(embedding)).to(device)
         assert len(embedding) == len(index2id) == len(corpus), f"{len(embedding)}, {len(index2id)}, {len(corpus)}"
         return index2id, corpus, embedding
 
-    logger = get_logger(__name__)
     query_index2id, query_corpus, query_embedding = load_index(query_path)
-    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-    logger.info(f"device: {device}")
     logger.info(f"load query: {query_embedding.shape}")
     if index_expansion_path:
         assert index_meta_embedding_path
@@ -100,7 +101,7 @@ def get_semantic_search_result(
                         v_expansions = torch.stack([index_expansion_embedding[i] for i in index_group[index_key]])
                         weight = torch.nn.functional.softmax(torch.inner(v_index, v_expansions))
                         v_index += torch.mm(weight, v_expansions)
-                    index_meta_embedding.append(v_index.numpy())
+                    index_meta_embedding.append(v_index.cpu().numpy())
                 np_save(np.concatenate(index_meta_embedding), filename)
             with open(f"{index_meta_embedding_path}/index2id.json", "w") as f:
                 json.dump(index_index2id, f)
@@ -113,8 +114,8 @@ def get_semantic_search_result(
         index_index2id, index_corpus, index_embedding = load_index(index_path)
         logger.info(f"load document: {index_embedding.shape}")
     search_result = semantic_search(
-        query_embedding.to(device),
-        index_embedding.to(device),
+        query_embedding,
+        index_embedding,
         query_chunk_size=query_chunk_size,
         corpus_chunk_size=corpus_chunk_size,
         top_k=k
